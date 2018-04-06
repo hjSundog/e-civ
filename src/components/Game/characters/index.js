3e54e8b18056fdf6d34fd66a536eb6312598f53d
breakinferno <1972952841@qq.com>
2018-04-06 22:01:29 +0800

    add e-civ-game

--- src/components/Game/characters/index.js
+++
@@ -1,5 +0,0 @@
-import Archer from './archer'
-import ThiefHead from './thiefHead'
-
-
-export default {Archer, ThiefHead}
--- src/views/Game/index.jsx
+++ src/views/Game/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Game from '@/components/Game'
+import Game from 'e-civ-game';
+import * as PIXI from 'pixi.js';
 
 const enemy = [{
     soldierType: 'Archer',
@@ -27,15 +28,61 @@ class GameScene extends React.Component {
     }
 
     componentDidMount() {
+        this.gs = new Game();
+        const data = JSON.parse(localStorage.getItem('rt')).data;
+        this.gs.setDriveFrames(data);
+        this.gs.mountAt(this.node);
+        this.gs.setBattleGround(800, 600, {
+            row: 5,
+            col: 20
+        });
+        // 设置背景色
+        this.gs.setBg(0x4f7dc4);
+        // 创建结束场景
+        this.gs.makeScene('gameOver', (pre, scene) => {
+            let style = new PIXI.TextStyle({
+                fontFamily: "Arial",
+                fontSize: 36,
+                fill: "white",
+                stroke: '#ff3300',
+                strokeThickness: 4,
+                dropShadow: true,
+                dropShadowColor: "#000000",
+                dropShadowBlur: 4,
+                dropShadowAngle: Math.PI / 6,
+                dropShadowDistance: 6,
+            });  
+            const txt = pre === 'my'?'恭喜你，我方赢了':'很遗憾,我方溃败!';
+            const message = new PIXI.Text(txt, style);
+            const center = this.gs.battleGround.getCenter();
+            message.anchor.set(0.5, 0.5);
+            message.position.set(center.x, center.y);
+            scene.addChild(message);
+        });
 
+        // 指定结束场景,及其回调
+        this.gs.overScene('gameOver', null, null, (result, scene, battleGround)=>{
+            console.log('所有的操作都在这里啦！')
+            console.log('gameOver 啦');
+        })
+
+        this.gs.load(()=>{
+            this.gs.setSoldiers({
+                soldiers: enemy, 
+                user: 'enemy'
+            }, 
+            {
+                soldiers: my, 
+                user: 'my'
+            });
+            this.gs.start('my');
+        })
     }
 
     render () {
         return (
             <div ref={(node)=>this.node = node}>
-                <Game enemy={enemy}
-                    my = {my}
-                />
+
             </div>
         )
     }
